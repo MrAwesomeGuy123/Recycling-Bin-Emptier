b67201dc190adef63a2084381cc5615b170b702a
MrAwesomeGuy123 <275166637+MrAwesomeGuy123@users.noreply.github.com>
2026-04-10 20:49:00 +0100

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Declare Function SHEmptyRecycleBin Lib "shell32.dll" Alias "SHEmptyRecycleBinA" (ByVal hwnd As Long, ByVal pszRootPath As String, ByVal dwFlags As Long) As Long
+    Declare Function SHUpdateRecycleBinIcon Lib "shell32.dll" () As Long
+
+    Const SHERB_NOCONFIRMATION = &H1
+    Const SHERB_NOPROGRESSUI = &H2
+    Const SHERB_NOSOUND = &H4
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            SHEmptyRecycleBin(0, Nothing, SHERB_NOCONFIRMATION Or SHERB_NOPROGRESSUI Or SHERB_NOSOUND)
+            SHUpdateRecycleBinIcon()
+            MessageBox.Show("Recycle Bin emptied!")
+        Catch ex As Exception
+            MessageBox.Show("How did you manage to error this, idiot.")
+        End Try
+    End Sub
+End Class
